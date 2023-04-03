61ff02a53a9e17f271134d7f763b2da28160bdb7
Ranishka Fernando <fernandoranishka@gmail.com>
2023-04-03 18:08:42 -0400

    Add personalized_hello function to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 def hello():
     return "Hello, DevOps!"
 
+
+def personalized_hello(name):
+    return f"Hello, {name}! Welcome to DevOps."
+
+
 if __name__ == "__main__":
     print(hello())
+    print(personalized_hello("John"))
