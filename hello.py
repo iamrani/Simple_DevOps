933355b33af2642807a4ec7f6fc566a001adcbe8
Ranishka Fernando <fernandoranishka@gmail.com>
2023-04-06 09:58:40 -0400

    Bug added

--- hello.py
+++ hello.py
@@ -5,7 +5,6 @@ def hello():
 def personalized_hello(name):
     return f"Hello, {name}! Welcome to DevOps."
 
-
 if __name__ == "__main__":
     print(hello())
     print(personalized_hello("Dominick Fournier"))
