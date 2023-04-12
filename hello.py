5a4813b62da6d76f907aec1d393eaab6a5970f8f
Ranishka Fernando <fernandoranishka@gmail.com>
2023-04-12 09:23:31 -0400

    Revert "Bug introduced"
    This reverts commit 67e74a3eebce7909728979d07fe3d0e2d4308173.

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ def hello():
 def personalized_hello(name):
     return f"Hello, {name}! Welcome to DevOps."
 
+
 if __name__ == "__main__":
     print(hello())
     print(personalized_hello("Ranishka  Nirmal Fernando"))
