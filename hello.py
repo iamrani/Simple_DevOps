3e0474d3b973b09df535be9ebdd16f5e06b410f1
Ranishka Fernando <fernandoranishka@gmail.com>
2023-09-26 22:29:32 -0400

    Updated the message

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 def hello():
-    return "Hello, DevOps!"
+    return "Hello, Telesat!"
 
 
 def personalized_hello(name):
-    return f"Hello, {name}! Welcome to DevOps."
+    return f"Hello, {name}! Welcome to the simple DevOps Tutorial."
 
 
 if __name__ == "__main__":
     print(hello())
-    print(personalized_hello("Ranishka Fernando"))
+    print(personalized_hello("Ranishka"))
